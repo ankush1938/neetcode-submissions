2cd71e3cd66f1d668dbcb206f3c7a8b31f769bfe
ankush1938 <ankushrana1938@gmail.com>
2026-05-03 23:09:56 +0530

    Add: balanced-binary-tree - submission-6

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-6.cs
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool IsBalanced(TreeNode root) {
+        
+        if (root == null){
+            return true;
+        }
+        int left = Height(root.left);
+        int right = Height(root.right);
+         if (Math.Abs(left - right) > 1) return false;
+        return IsBalanced(root.left) && IsBalanced(root.right);
+    }
+    private int Height(TreeNode root){
+        int count = 1;
+        if (root == null){
+            return count;
+        }
+        int left = 0;
+        int right = 0;
+        
+            left += Height(root.left);
+       
+            right += Height(root.right);
+        
+        if( left > right){
+            count += left;
+        }
+        else{
+            count += right;
+        }
+        return count;
+    }
+}
